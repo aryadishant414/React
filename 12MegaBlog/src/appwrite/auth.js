7367f87e22067bd58d6b15d3a83951159c0dedc3
dishant <aryadishant414@gmail.com>
2024-07-08 10:12:21 +0530

    Creating Authentication file in our Mega 'Blog' Project

---
+++ 12MegaBlog/src/appwrite/auth.js
@@ -0,0 +1,5 @@
+// This is our Authentication file.
+// auth yaha hamm authentication ko hee bol rhe hai
+
+import conf from '../conf/conf.js'  //Environment variable waali file ko import karwaya hai yaha
+
