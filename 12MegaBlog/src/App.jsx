3c3b0e9d969ef587d4f43a08be7ba17b28b68b73
dishant <aryadishant414@gmail.com>
2024-07-09 19:08:55 +0530

    Learned to create Production grade react Components

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -1,15 +1,39 @@
-
+import { useState , useEffect } from 'react'
+import {useDispatch} from 'react-redux'
 import './App.css'
+import authService from "./appwrite/auth.js"
+import {login , logout} from "./store/authSlice.js"
+import Header from "./components/Header/Header.jsx"
+import Footer from "./components/Footer/Footer.jsx"
 
 function App() {
+  const [loading , setLoading] = useState(true)
+  const dispatch = useDispatch()  //Actions perform krne ke liye isko use krte hai
+  
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if (userData) {
+        dispatch(login({userData}))
+      }
+      else {
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  } , [])
 
-  console.log(import.meta.env.VITE_APPWRITE_URL)
-
-  return (
-    <>
-     <h1>A blog app with Appwrite</h1>
-    </>
-  )
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          TODO :{/* <Outlet /> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
